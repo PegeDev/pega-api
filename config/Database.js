668176e861a18c38513e7da3465c672a54855269
PegeDev <pege.dev7878@gmail.com>
2022-11-12 18:37:58 +0700

    change db

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("tiktok_database", "root", "ciamis1221@", {
-  host: "localhost",
+const db = new Sequelize("tiktok_database", "root", "Ciamis1221@", {
+  host: "127.0.0.1",
   dialect: "mysql",
 });
 
