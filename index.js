6baa042e4b5442fe5ad824b8ecc377a1112227ef
PegeDev <pege.dev7878@gmail.com>
2022-11-12 17:59:57 +0700

    add route for downloading

--- index.js
+++ index.js
@@ -4,38 +4,39 @@ const session = require("express-session");
 const cors = require("cors");
 const CronJob = require("./controllers/cron.js");
 const tiktokRoute = require("./router/tiktok");
+const downloadsRoute = require("./router/download");
 const authRoute = require("./router/auth");
 const db = require("./config/Database.js");
 const bodyParser = require("body-parser");
 const Users = require("./models/UserModels.js");
 
-
 function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const port  = getRandomInt(5000, 8000)
+const port = getRandomInt(5000, 8000);
 
 dotenv.config();
 
 (async () => {
-	await db.sync()
-})()
+  await db.sync();
+})();
 const app = express();
 app.use(express.json());
 app.use(
-	cors({
-		credentials: true,
-	})
+  cors({
+    credentials: true,
+  })
 );
 
 app.use(tiktokRoute);
 app.use(authRoute);
+app.use(downloadsRoute);
 
 app.listen(5000, () => {
-	console.log(`Node Started with `+5000);
+  console.log(`Node Started with ` + 5000);
 });
 
 CronJob.initScheduleCronJobs();
---
+++ router/download.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { verifyToken } = require("../middleware/Verify");
+const router = express.Router();
+
+router.get("/download/:file", verifyToken, async (req, res) => {
+  const filePath = (await __dirname) + "/media" + req.params.file;
+  await res.download(
+    filePath,
+    req.params.file, // Remember to include file extension
+    (err) => {
+      if (err) {
+        res.send({
+          error: err,
+          msg: "Problem downloading the file",
+        });
+      }
+    }
+  );
+});
+
+module.exports = router;
