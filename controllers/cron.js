b6a30ac0ce8f2a5759f1b6a3263851bc22e9c0b0
PegeDev <pege.dev7878@gmail.com>
2022-11-15 19:40:48 +0700

    cron fixed

--- controllers/cron.js
+++ controllers/cron.js
@@ -2,21 +2,21 @@ const cronJob = require("node-cron");
 const fs = require("fs");
 
 exports.initScheduleCronJobs = () => {
-	const cronJobsSchedule = cronJob.schedule("0 23 * * 1-7", () => {
-		const cek = fs.readdirSync("./media");
-		console.log(new Date().toLocaleString());
-		if (cek.length >= 0) {
-			for (var file of cek) {
-				fs.unlink(`./media/${file}`, (err) => {
-					if (err) return err;
-					if (!err) return console.log("suksess delete file ", file);
-				});
-			}
-		} else {
-			console.log("Dir is Empty!");
-		}
-	});
-	console.log("Cron Jobs Started!");
+  const cronJobsSchedule = cronJob.schedule("0 0 0 1/1 * ? *", () => {
+    const cek = fs.readdirSync("./media");
+    console.log(new Date().toLocaleString());
+    if (cek.length > 0) {
+      for (var file of cek) {
+        fs.unlink(`./media/${file}`, (err) => {
+          if (err) return err;
+          if (!err) return console.log("suksess delete file ", file);
+        });
+      }
+    } else {
+      console.log("Dir is Empty!");
+    }
+  });
+  console.log("Cron Jobs Started!");
 
-	cronJobsSchedule.start();
+  cronJobsSchedule.start();
 };
